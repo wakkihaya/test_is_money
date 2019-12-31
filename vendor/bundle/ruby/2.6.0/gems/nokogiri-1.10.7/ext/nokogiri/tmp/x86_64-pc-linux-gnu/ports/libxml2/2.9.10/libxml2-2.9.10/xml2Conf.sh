ffc135a298e6841499bad6ca7db2e9fc35d52fe9
wakkihaya <wakkihaya@gmail.com>
2019-12-31 11:20:37 +0900

    rails アプリ初期環境

---
+++ vendor/bundle/ruby/2.6.0/gems/nokogiri-1.10.7/ext/nokogiri/tmp/x86_64-pc-linux-gnu/ports/libxml2/2.9.10/libxml2-2.9.10/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/mnt/c/Users/Hayato Waki/RubymineProjects/test_is_money/testIsMoney/vendor/bundle/ruby/2.6.0/gems/nokogiri-1.10.7/ports/x86_64-pc-linux-gnu/libxml2/2.9.10/lib"
+XML2_LIBS="-lxml2 -lz     -lm "
+XML2_INCLUDEDIR="-I/mnt/c/Users/Hayato Waki/RubymineProjects/test_is_money/testIsMoney/vendor/bundle/ruby/2.6.0/gems/nokogiri-1.10.7/ports/x86_64-pc-linux-gnu/libxml2/2.9.10/include/libxml2"
+MODULE_VERSION="xml2-2.9.10"
+
